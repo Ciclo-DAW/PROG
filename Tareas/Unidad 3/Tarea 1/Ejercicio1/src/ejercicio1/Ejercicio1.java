3612df3954210f5eacfa474826528602cf268ae7
Brais <braiscao.dev@gmail.com>
2020-01-11 18:49:53 +0100

    Ejercicio 1

---
+++ Tareas/Unidad 3/Tarea 1/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,10 @@
+package ejercicio1;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        Libro l1 = new Libro("asdfasdf", "Libro de Prueba", "Paco Perez", 5);
+        l1.visualizaDatosLibro();
+    }
+    
+}
--- Tareas/Unidad 4/Tarea 1/Ejercicio5/src/ejercicio5/Ejercicio5.java
+++ Tareas/Unidad 4/Tarea 1/Ejercicio5/src/ejercicio5/Ejercicio5.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ejercicio5;
-import java.util.Scanner;
+        import java.util.Scanner;
 /**
  *
  * @author Brais
@@ -38,5 +38,5 @@ public class Ejercicio5 {
 
         }
     }
-    
+
 }
