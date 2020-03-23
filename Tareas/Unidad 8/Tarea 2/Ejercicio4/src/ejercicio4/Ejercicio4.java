74583e37a09ce654740a0aee05e573ea6055f92c
Brais <braiscao.dev@gmail.com>
2020-03-23 18:57:59 +0100

    Ejercicio 4

--- Tareas/Unidad 8/Tarea 2/Ejercicio4/src/ejercicio4/Ejercicio4.java
+++ Tareas/Unidad 8/Tarea 2/Ejercicio4/src/ejercicio4/Ejercicio4.java
@@ -5,17 +5,52 @@
  */
 package ejercicio4;
 
-/**
- *
- * @author Brais
- */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Ejercicio4 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArrayList<Integer> valores = new ArrayList<Integer>();
+        ArrayList<Integer> numeros;
+        Scanner teclado = new Scanner(System.in);
+        int size;
+        int valor;
+        int pos;
+        int aux;
+
+        valor = teclado.nextInt();
+        while(valor != 0){
+            valores.add(valor);
+            valor = teclado.nextInt();
+        }
+
+        for(int i = 0; i<valores.size(); i++){
+            size = valores.get(i);
+            numeros = new ArrayList<Integer>();
+            pos = 0;
+            aux = 1;
+            for(int j = 1; j<= size; j++){
+                numeros.add(j);
+            }
+
+            while(aux < numeros.size()){
+                while(pos < numeros.size()){
+                    numeros.remove(pos);
+                    pos = pos + aux;
+                }
+                pos = 0;
+                aux++;
+            }
+
+            Collections.sort(numeros, Collections.reverseOrder());
+            System.out.print(size + ": ");
+            for(int j = 0; j<numeros.size(); j++){
+                System.out.print(numeros.get(j) + " ");
+            }
+
+            System.out.println();
+        }
     }
-    
 }
