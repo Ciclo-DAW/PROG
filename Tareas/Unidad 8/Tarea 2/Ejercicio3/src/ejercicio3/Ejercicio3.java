682df887abd72fdea2e6b8bdf86dd0b0a3a42138
Brais <braiscao.dev@gmail.com>
2020-03-23 18:06:37 +0100

    Ejercicio 3

--- Tareas/Unidad 8/Tarea 2/Ejercicio3/src/ejercicio3/Ejercicio3.java
+++ Tareas/Unidad 8/Tarea 2/Ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -5,6 +5,9 @@
  */
 package ejercicio3;
 
+import java.util.Random;
+import java.util.TreeSet;
+
 /**
  *
  * @author Brais
@@ -15,7 +18,38 @@ public class Ejercicio3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[] numeros = new int[50];
+        int maximo = 1;
+        int minimo = 20;
+        int cont = 1;
+        Random rand = new Random();
+
+        TreeSet<Integer> pares = new TreeSet<Integer>();
+
+
+        for(int i = 0; i<numeros.length; i++){
+            numeros[i] = rand.nextInt(20) + 1;
+            if(numeros[i] > maximo) maximo = numeros[i];
+            if(numeros[i] < minimo) minimo = numeros[i];
+
+            if(numeros[i]%2 == 0 && !pares.contains(numeros[i])){
+                pares.add(numeros[i]);
+            }
+
+            if(cont < 10){
+                System.out.print(numeros[i] + ", ");
+                cont++;
+            } else if (cont == 10){
+                System.out.print(numeros[i]);
+                System.out.println();
+                cont = 1;
+            }
+        }
+
+        System.out.println("El número más grande es: " + maximo);
+        System.out.println("El número más pequeño es: " + minimo);
+
+        System.out.println(pares);
+
     }
-    
 }
