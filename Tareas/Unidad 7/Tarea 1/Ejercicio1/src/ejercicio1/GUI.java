9a38d1955f737612faf80f3d2f7228496e10f97a
Brais <braiscao.dev@gmail.com>
2020-05-03 21:18:42 +0200

    Unidad7 Tarea 1 ejercicio 1

--- Tareas/Unidad 7/Tarea 1/Ejercicio1/src/ejercicio1/GUI.java
+++ Tareas/Unidad 7/Tarea 1/Ejercicio1/src/ejercicio1/GUI.java
@@ -16,6 +16,7 @@ public class GUI extends javax.swing.JFrame {
      */
     public GUI() {
         initComponents();
+
     }
 
     /**
@@ -27,21 +28,129 @@ public class GUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        texto = new javax.swing.JTextArea();
+        numberA = new javax.swing.JTextField();
+        numberE = new javax.swing.JTextField();
+        numberI = new javax.swing.JTextField();
+        numberO = new javax.swing.JTextField();
+        numberU = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        buttonContar = new javax.swing.JButton();
+        buttonLimpiar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Contar vocales");
+        setResizable(false);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel1.setText("Introduce el texto");
+
+        texto.setColumns(20);
+        texto.setRows(5);
+        jScrollPane1.setViewportView(texto);
+
+        jLabel2.setText("Número de A/a:");
+
+        jLabel3.setText("Número de E/e:");
+
+        jLabel4.setText("Número de I/i:");
+
+        jLabel5.setText("Número de O/o:");
+
+        jLabel6.setText("Número de U/u:");
+
+        buttonContar.setText("Contar");
+        buttonContar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonContarActionPerformed(evt);
+            }
+        });
+
+        buttonLimpiar.setText("Limpiar Texto");
+        buttonLimpiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonLimpiarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 374, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel6))
+                        .addGap(51, 51, 51)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(numberO, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 34, Short.MAX_VALUE)
+                            .addComponent(numberI, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(numberE, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(numberA, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(numberU))
+                        .addGap(33, 33, 33)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(buttonContar, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(buttonLimpiar, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(47, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(numberA, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(numberE, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3)
+                    .addComponent(buttonContar))
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(numberI, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4)
+                    .addComponent(buttonLimpiar))
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(numberO, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5))
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(numberU, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel6))
+                .addContainerGap(28, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void buttonContarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonContarActionPerformed
+        contarVocales(texto.getText());
+    }//GEN-LAST:event_buttonContarActionPerformed
+
+    private void buttonLimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonLimpiarActionPerformed
+        limpiarCampos();
+    }//GEN-LAST:event_buttonLimpiarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -77,7 +186,63 @@ public class GUI extends javax.swing.JFrame {
             }
         });
     }
+    
+    public void contarVocales(String t){
+        int numA, numE, numI, numO, numU;
+        numA = numE = numI = numO = numU = 0;
+        for(int i = 0; i < t.length(); i++){
+            switch(Character.toLowerCase(t.charAt(i))) {
+                case 'a': 
+                    numA++;
+                    break;
+                case 'e': 
+                    numE++;
+                    break;
+                case 'i': 
+                    numI++;
+                    break;
+                case 'o': 
+                    numO++;
+                    break;
+                case 'u': 
+                    numU++;
+                    break;
+            }
+        }
+        numberA.setText(numA + "");
+        numberE.setText(numE + "");
+        numberI.setText(numI + "");
+        numberO.setText(numO + "");
+        numberU.setText(numU + "");
+
+    } 
+    
+    public void limpiarCampos(){
+       
+        numberA.setText("");
+        numberE.setText("");
+        numberI.setText("");
+        numberO.setText("");
+        numberU.setText("");
+        
+        texto.setText("");
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton buttonContar;
+    private javax.swing.JButton buttonLimpiar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField numberA;
+    private javax.swing.JTextField numberE;
+    private javax.swing.JTextField numberI;
+    private javax.swing.JTextField numberO;
+    private javax.swing.JTextField numberU;
+    private javax.swing.JTextArea texto;
     // End of variables declaration//GEN-END:variables
 }
